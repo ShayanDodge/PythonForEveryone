e3c5a4f1a46e9a4fa7bab41fa868de3c346ace89
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2023-04-16 13:14:57 +0330

    Update P1_10.py
    Modified 1

--- P1_10.py
+++ P1_10.py
@@ -1,3 +1,4 @@
 # P1.10 Write a program that print an animal speaking a greeting, similar to the following.
 print("/", "\ _", "/", "\ ", "  ", "-------")
 print("(", " ' ' ", ")", " ", "/ Hello \ ")
+print("/", "\ _", "/", "\ ", "  ", "-------")
