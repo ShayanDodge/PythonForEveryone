2d1c90c32ceb34cdd89324cb302726b18f19425d
Shayan Dodge <94797491+ShayanDodge@users.noreply.github.com>
2023-04-16 13:13:47 +0330

    Add files via upload
    P1.10

---
+++ P1_10.py
@@ -0,0 +1,3 @@
+# P1.10 Write a program that print an animal speaking a greeting, similar to the following.
+print("/", "\ _", "/", "\ ", "  ", "-------")
+print("(", " ' ' ", ")", " ", "/ Hello \ ")
