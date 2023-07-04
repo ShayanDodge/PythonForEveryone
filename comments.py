6ccd52ac88ebc299c4ca45dff504cabbfed12853
shayan <dodgeshayan@gmail.com>
2023-07-04 11:47:38 +0330

    1

---
+++ comments.py
@@ -0,0 +1,14 @@
+"""
+This is my first python code
+This is hello world
+"""
+# print("shayan","omid","ali",sep="...&...",end=" ! ")
+# print("shayan","omid","ali",sep="...&...")
+# print("shayan","omid","ali",sep="...&...",end=" ? ")
+# print("shayan","omid","ali",sep="...&...")
+
+print("*   *   **   ****   ****   *   *")
+print("*   *  *  *  *   *  *   *  *   *")
+print("***** *    * ****   ****    * *")
+print("*   * ****** *   *  *   *    *")
+print("*   * *    * *    * *    *   *")
